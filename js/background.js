11f2b518191443d22a46099f5b771df614a98a8e
wangfpp <wangfpp@gmail.com>
2020-12-15 14:29:03 +0800

    add: 增加时间显示和搜索功能

--- js/background.js
+++ js/background.js
@@ -1,1 +1,27 @@
+window.onload = _ => {
+    let date_node = document.querySelector('.date');
+    let input_node = document.querySelector('#search');
+    if (date_node) {
+        moment.locale('zh-cn');
+        let base_parse = moment().format("LLLL");
+        date_node.innerHTML = base_parse;
+        setInterval(() => {
+            if (date_node) {
+                base_parse = moment().format("LLLL");
+                date_node.innerHTML = base_parse;
+            }
+        }, 1000);
+    }
+
+    input_node.onkeydown = function (e) {
+        let { keyCode, target } = e;
+        let { value } = target;
+        if (keyCode === 13 && value) {
+            window.location.href = `https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=${value}`
+        }
+    }
+    
+}
+
+
 chrome.browserAction.onClicked.addListener(() => chrome.tabs.create({}))
