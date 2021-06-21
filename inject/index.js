9f0dd51e7b0300b02a0d9cfc537076c73065ebdf
王金彪 <wangjinbiao@haier.com>
2021-06-21 16:40:44 +0800

    add: 支持自动暗黑主题

---
+++ inject/index.js
@@ -0,0 +1,16 @@
+(function() {
+    let css  = `html, img {
+        filter: invert(1) hue-rotate(.5turn);
+    }
+    img {
+        opacity: .75;    
+    }`
+    let cssStyle = document.createElement("style");
+    cssStyle.textContent = css;
+    if (document.head) {
+        document.head.append(cssStyle);
+    } else {
+        const root = document.documentElement;
+        root.append(cssStyle);
+    }
+})();
